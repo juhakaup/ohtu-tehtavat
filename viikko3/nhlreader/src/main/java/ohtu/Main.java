afa87c2ee8d087480aab44b26e42b4661ea96155
Juha Kauppinen <juha.kauppinen@helsinki.fi>
2020-11-11 09:53:15 +0200

    Players sorted, formatted output

--- viikko3/nhlreader/src/main/java/ohtu/Main.java
+++ viikko3/nhlreader/src/main/java/ohtu/Main.java
@@ -4,10 +4,14 @@ package ohtu;
 import com.google.gson.Gson;
 import org.apache.http.client.fluent.Request;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
+        
         String url = "https://nhlstatisticsforohtu.herokuapp.com/players";
 
         String bodyText = Request.Get(url).execute().returnContent().asString();
@@ -15,12 +19,21 @@ public class Main {
         Gson mapper = new Gson();
         Player[] players = mapper.fromJson(bodyText, Player[].class);
 
-        System.out.println("Players from FIN");
+        ArrayList<Player> playersFIN = new ArrayList<>();
+
+        System.out.println("Players from FIN\n");
         for(Player p : players) {
             if (p.getNationality().equals("FIN")) {
-                System.out.println(p);
+                playersFIN.add(p);
             }
         }
+        Collections.sort(playersFIN, Collections.reverseOrder());
+
+        for (Player p : playersFIN) {
+            String format = "%-20s %3s %2d + %2d = %2d %n";
+            System.out.printf(format, p.getName(), p.getTeam(), p.getAssists(), p.getClass(), p.getScore());
+        }
+        
     }
     
 }
